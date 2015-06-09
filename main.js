faec66188c1652aa2c72a1ddd9aa44a2db57571e
Christophe <meumeu1402@gmail.com>
2015-06-09 17:49:20 +0200

    can now play audio files

--- main.js
+++ main.js
@@ -25,9 +25,22 @@ function sayin(req, res, next) {
     next();
 }
 
+function play(req, res, next) {
+  
+    exec('aplay "/srv/say/sounds/' + req.params.file + '"', function callback(error, stdout, stderr){
+
+        if(error)
+            console.log("Error");
+    });
+
+    res.send('Playing sound : ' + req.params.file);
+    next();
+}
+
 var server = restify.createServer();
 server.get('/say/:sentence', say);
 server.get('/sayin/:lg/:sentence', sayin);
+server.get('/play/:file', play);
 
 server.listen(3333, function() {
   console.log('%s listening at %s', server.name, server.url);
